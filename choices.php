aaee950266b662b29ac04eec2aa1dd6a8fb68536
Klinke <klinke_klanke92@hotmail.com>
2018-10-31 19:50:24 +0100

    It's alive

---
+++ choices.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<p>
+You chose to be <?php echo $_POST["turtle"]; ?>.<br />
+Your weapon of choice is the <?php echo $_POST["weapon"]; ?>. <br /> 
+The color of your bandana will be <?php echo $_POST["bandana"]; ?>.
+</p>
+</body>
+</html> 
